239f5fdd3eabde34704cb8e1d7bd3c0522091e73
K-A-Watts <purchasing@kawatts.co.uk>
2024-04-09 10:54:21 +0100

    Adding all current content

---
+++ script.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+  document.getElementsByClassName("subLogos")[0].style.display = "flex";
+}, 500);
+
+setTimeout(function() {
+  document.getElementsByClassName("chart")[0].style.display = "block";
+}, 1000);
