8ff8432bba2a0629ef42acbd658c9173851e0645
K-A-Watts <purchasing@kawatts.co.uk>
2024-04-10 13:43:53 +0100

    Testing timings

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 setTimeout(function() {
+  document.getElementsByClassName("subLogos")[0].style.display = "none";
+}, 1);
+
+setTimeout(function() {
+  document.getElementsByClassName("chart")[0].style.display = "none";
+}, 1);
+
+setTimeout(function() {
   document.getElementsByClassName("subLogos")[0].style.display = "flex";
-}, 500);
+}, 750);
 
 setTimeout(function() {
   document.getElementsByClassName("chart")[0].style.display = "block";
-}, 1000);
+}, 1250);
